23c6c1d0693fe2a0cc3fab00746af76c536c66b8
zahra <zahraeng369@gmail.com>
2023-12-09 22:11:09 +0330

    completed user authentication

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 import { NavLinks } from "@/constant";
 import AuthProviders from "./AuthProviders";
+import { getCurrentUser } from "@/lib/session";
+import { signOut } from "next-auth/react";
+import ProfileMenu from "./ProfileMenu";
 
-const Navbar = () => {
-  const session = null;
+const Navbar = async () => {
+  const session = await getCurrentUser();
   return (
     <nav className="flex justify-between navbar">
       <div className="flex-1 flexStart gap-10">
@@ -20,9 +23,10 @@ const Navbar = () => {
         </ul>
       </div>
       <div className="flexCenter gap-4">
-        {session ? (
+        {session?.user ? (
           <>
-            UserPhoto
+            <ProfileMenu session={session} />
+
             <Link href="/create-project">Share Work</Link>
           </>
         ) : (
--- grafbase.config.ts
+++ grafbase.config.ts
@@ -1,31 +1,50 @@
-import { g, config } from '@grafbase/sdk'
+import { g, config, auth } from "@grafbase/sdk";
 
-const User = g.model('User',{
-  name:g.string().length({min:2,max:20}),
-  email:g.string().unique(),
-  avatarUrl:g.url(),
-  description:g.string().optional(),
-  githubUrl:g.url().optional(),
-  linledInUrl:g.url().optional(),
-  projects:g.relation(()=>Project).list().optional(),
-})
+// @ts-ignore
+const User = g
+  .model("User", {
+    name: g.string().length({ min: 2, max: 20 }),
+    email: g.string().unique(),
+    avatarUrl: g.url(),
+    description: g.string().optional(),
+    githubUrl: g.url().optional(),
+    linledInUrl: g.url().optional(),
+    projects: g
+      .relation(() => Project)
+      .list()
+      .optional(),
+  })
+  .auth((rules) => {
+    rules.public().read();
+  });
 
-const Project = g.model('Project',{
-  title:g.string().length({min:3}),
-  description:g.string(),
-  image:g.url(),
-  liveSiteUrl:g.url(),
-  category:g.string().search(),
-  createdBy:g.relation(()=>User)
-})
+// @ts-ignore
+const Project = g
+  .model("Project", {
+    title: g.string().length({ min: 3 }),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    category: g.string().search(),
+    createdBy: g.relation(() => User),
+  })
+  .auth((rules) => {
+    rules.public().read();
+    rules.private().create().delete().update();
+  });
+
+const jwt = auth.JWT({
+  issuer: "grafbase",
+  secret: process.env.NEXTAUTH_SECRET as string,
+});
 
 export default config({
   schema: g,
- 
-  // auth: {
-  //   rules: (rules) => {
-  //     rules.public()
-  //   },
-  // },
 
-})
+  auth: {
+    providers: [jwt],
+    rules: (rules) => {
+      rules.private();
+    },
+  },
+});
